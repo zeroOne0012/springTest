e6b2afc8f0a6f9d20e341dd1269155c13efcbfde
zeroone0012 <012012012il@gmail.com>
2024-12-24 05:13:31 +0900

    테스트 코드 통과
    스페이스바 문제였다~

--- src/main/java/com/example/FirstProject/repository/CommentRepository.java
+++ src/main/java/com/example/FirstProject/repository/CommentRepository.java
@@ -9,8 +9,8 @@ import java.util.List;
 public interface CommentRepository extends JpaRepository<Comment, Long> { // CrudRepository는 못하는 페이지 처리, sorting 처리 가능
     // 특정 게시글의 모든 댓글 조회
     @Query(value =
-            "SELECT *" +
-            "FROM comment" +
+            "SELECT * " +
+            "FROM comment " +
             "WHERE article_id = :articleId",
     nativeQuery = true)
     List<Comment> findByArticleId(Long articleId);
--- src/test/java/com/example/FirstProject/repository/CommentRepositoryTest.java
+++ src/test/java/com/example/FirstProject/repository/CommentRepositoryTest.java
@@ -24,26 +24,21 @@ class CommentRepositoryTest {
     void findByArticleId() {
         /* Case 1: 4번 게시글의 모든 댓글 조회 */
 
-//            // 입력 데이터 준비
-//            Long articleId = 4L;
-//
-//            // 실제 수행
-//            List<Comment> comments = commentRepository.findByArticleId(articleId);
-//
-//            // 예상하기
-//            Article target = new Article(4L, "당신의 인생 영화는?", "댓글로 알려주세요");
-//            Comment a = new Comment(1L, target, "Kim", "겨울왕국");
-//            Comment b = new Comment(2L, target, "Lee", "즐겁다");
-//            Comment c = new Comment(3L, target, "Park", "꺄르륵");
-//            List<Comment> expected = Arrays.asList(a, b, c);
-//
-//            // 검증
-//            assertEquals(expected.toString(), comments.toString());
+            // 입력 데이터 준비
+            Long articleId = 4L;
 
-            String expected = null;
-            String comments = null;
-            assertEquals(expected, comments);
+            // 실제 수행
+            List<Comment> comments = commentRepository.findByArticleId(articleId);
 
+            // 예상하기
+            Article target = new Article(4L, "당신의 인생 영화는?", "댓글로 알려주세요");
+            Comment a = new Comment(1L, target, "Kim", "겨울왕국");
+            Comment b = new Comment(2L, target, "Lee", "라푼젤");
+            Comment c = new Comment(3L, target, "Park", "라퓨타");
+            List<Comment> expected = Arrays.asList(a, b, c);
+
+            // 검증
+            assertEquals(expected.toString(), comments.toString());
 
 
     }
